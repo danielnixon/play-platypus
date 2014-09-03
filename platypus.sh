2a5214987857fb4a075b76a117c25f8f17e7f9fb
Daniel Nixon <dan.nixon@gmail.com>
2014-09-03 18:19:27 +1000

    Update platypus.sh

--- platypus.sh
+++ platypus.sh
@@ -8,12 +8,10 @@ set -e
 
 command -v platypus >/dev/null 2>&1 || { echo "Platypus is required but not installed." >&2; exit 1; }
 
-APP_NAME=`activator -Dsbt.log.noformat=true name | tail -n 1 | cut -d " " -f 2`
-VERSION=`activator -Dsbt.log.noformat=true version | tail -n 1 | cut -d " " -f 2`
+APP_NAME=$1
+VERSION=$2
 DMG_NAME="$APP_NAME-$VERSION"
 
-# Create tarball.
-activator universal:package-zip-tarball
 cd ./target/universal/
 
 # Remove previous DMG image if it exists.
@@ -34,7 +32,7 @@ fi
 
 mkdir $APP_NAME
 ln -s /Applications $APP_NAME/Applications
-platypus -V "$VERSION" -a "$APP_NAME" -o "$OUTPUT_TYPE" $ICON_ARG -f share -f bin -f conf -f lib run.sh "$APP_NAME/$APP_NAME.app"
+platypus -V "$VERSION" -a "$APP_NAME" -o "$OUTPUT_TYPE" $ICON_ARG -f bin -f conf -f lib run.sh "$APP_NAME/$APP_NAME.app"
 
 # Create DMG image.
 hdiutil create -srcfolder $APP_NAME ../$DMG_NAME.dmg
